6e3235e89448ee71ddb6aac60648c5296d31685c
TheNewC0der-24 <191b303@juetguna.in>
2021-09-07 14:38:47 +0530

    Palindrome Checker

---
+++ JS/script.js
@@ -0,0 +1,24 @@
+console.log('Welcome to Palindrome Checker');
+
+const btnCheck = document.querySelector("button");
+const str = document.querySelector("input");
+
+btnCheck.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const inputWord = str.value.toLocaleLowerCase();
+    const string = inputWord.split("");
+
+    const revString = string.reverse();
+    const joinString = revString.join("");
+
+    if (inputWord == joinString) {
+        document.querySelector(".output-text").innerHTML = `<div>
+                                                              <p class="text-center" style="font-size: 1rem; color: black;">${inputWord} = ${joinString}</p>
+                                                              <p class="text-center text-success" style="font-size: 1.5rem; color: black;">It is a Palindrome !!</p>`;
+
+        str.value = "";
+    } else {
+        document.querySelector(".output-text").innerHTML = ` <p class="text-center text-danger my-5" style="font-size: 1.25rem; color: black;">It is not a Palindrome !!</p>`;
+    }
+});
